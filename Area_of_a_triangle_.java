7b8d9231320d7a19550c38aab6516b0eb9d4b474
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-06 18:14:55 +0530

    import java.util.*; public class h{     public static void main(String a[])     {         Scanner s=new Scanner(System.in);         int l=s.nextInt();         int b=s.nextInt();         System.out.printf("%d",(l*b)/2);     }
    }

---
+++ Area_of_a_triangle_.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class h{
+    public static void main(String a[])
+    {
+        Scanner s=new Scanner(System.in);
+        int l=s.nextInt();
+        int b=s.nextInt();
+        System.out.printf("%d",(l*b)/2);
+    }
+    
+}
