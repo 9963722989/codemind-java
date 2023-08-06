4f84a4bb50c604b794cce2ae19613b095479767a
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-06 18:11:03 +0530

    import java.util.*; public class h{     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int v=s.nextInt();         int v1=v%100;         System.out.printf("%02d",v1);     } }

---
+++ _Last_two_Digits_of_a_given_Year.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class h{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int v=s.nextInt();
+        int v1=v%100;
+        System.out.printf("%02d",v1);
+    }
+}
