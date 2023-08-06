1374bed6200467c005fc3ac65a593d9212242d2d
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-06 22:00:02 +0530

    import java.util.*; public class h{     public static void main(String a[])     {         Scanner s=new Scanner(System.in);         float v=s.nextInt(),v1=s.nextInt(),v2=s.nextInt();         float v3=((v+v1)*v2)/2;         System.out.printf("%.4f",v3);     } }

---
+++ Program_to_calculate_the_area_of_Trapezium.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class h{
+    public static void main(String a[])
+    {
+        Scanner s=new Scanner(System.in);
+        float v=s.nextInt(),v1=s.nextInt(),v2=s.nextInt();
+        float v3=((v+v1)*v2)/2;
+        System.out.printf("%.4f",v3);
+    }
+}
