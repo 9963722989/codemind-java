c74e6862e5175e32de8c0778708e5b2955128f85
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-06 22:29:26 +0530

    import java.util.*; public class h{     public static void main(String ai[])     {         Scanner s=new Scanner(System.in);         int a=s.nextInt(),b=s.nextInt(),d=s.nextInt();         int c=5*a+10*b;         System.out.printf("%d",c/d);     } }

---
+++ Romeo_and_Juliet.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class h{
+    public static void main(String ai[])
+    {
+        Scanner s=new Scanner(System.in);
+        int a=s.nextInt(),b=s.nextInt(),d=s.nextInt();
+        int c=5*a+10*b;
+        System.out.printf("%d",c/d);
+    }
+}
