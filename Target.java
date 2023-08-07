cf6c78210df522ff22a4c75bd26de02cfd50346f
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-07 15:31:21 +0530

    import java.util.*; public class h{     public static void main(String[] a)     {         Scanner s=new Scanner(System.in);         int v=s.nextInt(),v1=s.nextInt(),v2=s.nextInt(),v3=s.nextInt();         if(v>=10&v1>=10&v2>=10&v3>=10)         {            System.out.print("YES");         }         else         {             System.out.printf("NO");         }     } }

---
+++ Target.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class h{
+    public static void main(String[] a)
+    {
+        Scanner s=new Scanner(System.in);
+        int v=s.nextInt(),v1=s.nextInt(),v2=s.nextInt(),v3=s.nextInt();
+        if(v>=10&v1>=10&v2>=10&v3>=10)
+        {
+           System.out.print("YES"); 
+        }
+        else
+        {
+            System.out.printf("NO");
+        }
+    }
+}
