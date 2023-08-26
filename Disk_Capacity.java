e97f0f02978499beb4f14cb6b659d9fa5ef71a8d
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-26 21:54:50 +0530

    import java.util.*; public class h{     public static void main(String[] Arg)     {         Scanner s=new Scanner(System.in);         int n=s.nextInt(),m=s.nextInt(),w=s.nextInt();         System.out.print((n*m*w)*1024);     } }

---
+++ Disk_Capacity.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class h{
+    public static void main(String[] Arg)
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt(),m=s.nextInt(),w=s.nextInt();
+        System.out.print((n*m*w)*1024);
+    }
+}
