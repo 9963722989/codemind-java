ee72facbf35d1edae88c1659bfd1c420114fe8a9
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-31 20:16:49 +0530

    import java.util.*; public class h{     public static void main(String[] Args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int d=s.nextInt();         System.out.print(n*d);     }
    }

---
+++ Multiplication.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class h{
+    public static void main(String[] Args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int d=s.nextInt();
+        System.out.print(n*d);
+    }
+
+    
+    
+}
