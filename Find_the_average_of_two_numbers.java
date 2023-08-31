1a440ab357f411a941e13d10915f0c08f7d59e58
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-31 20:30:59 +0530

    import java.util.*; public class h{     public static void main(String[] Args){         Scanner s=new Scanner(System.in);         float n=s.nextInt();         float d=s.nextInt();         System.out.printf("%.4f",(n+d)/2);     }
    }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class h{
+    public static void main(String[] Args){
+        Scanner s=new Scanner(System.in);
+        float n=s.nextInt();
+        float d=s.nextInt();
+        System.out.printf("%.4f",(n+d)/2);
+    }
+
+    
+    
+}
