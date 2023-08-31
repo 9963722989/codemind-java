889708504fa747d3e6b842d4729a6f608b697285
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-31 19:29:41 +0530

    import java.util.*; public class h{     public static void main(String[] Args){         Scanner s=new Scanner(System.in);         float n=s.nextInt();         System.out.printf("%.2f",32+(n*(9)/(5)));     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class h{
+    public static void main(String[] Args){
+        Scanner s=new Scanner(System.in);
+        float n=s.nextInt();
+        System.out.printf("%.2f",32+(n*(9)/(5)));
+    }
+}
