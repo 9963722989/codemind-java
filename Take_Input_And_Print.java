ff8eb08d0fb444a02e2d9ca597585607facdac94
Sangeetha Subhash <123374762+9963722989@users.noreply.github.com>
2023-08-31 20:10:50 +0530

    import java.util.*; public class h{     public static void main(String[] Args){         Scanner s=new Scanner(System.in);         String n=s.nextLine();         System.out.println(n);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class h{
+    public static void main(String[] Args){
+        Scanner s=new Scanner(System.in);
+        String n=s.nextLine();
+        System.out.println(n);
+    }
+}   
